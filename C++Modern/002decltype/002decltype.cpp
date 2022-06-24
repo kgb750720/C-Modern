db8f54893247bfb3d30d423a5a0b4885a73f1cbf
shengyupian <shengyupian@SHENGYUPIAN>
2022-06-24 19:50:37 +0800

    右值引用（完

---
+++ C++Modern/002decltype/002decltype.cpp
@@ -0,0 +1,75 @@
+﻿// 002decltype.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+/********** typeid 关键字**************/
+void sample1()
+{
+    //typeid可以在运行时获取类型信息
+    int x1 = 0;
+    double x2 = 1.1;
+    //获取类型名称
+    cout << typeid(x1).name() << endl;
+    cout << typeid(x1 + x2).name() << endl;
+    cout << typeid(int).name() << endl;
+
+    //简化名信息(比如.N表示int .H表示double 
+    cout << typeid(x1).raw_name() << endl;
+    cout << typeid(x1 + x2).raw_name() << endl;
+    cout << typeid(int).raw_name() << endl;
+}
+/********** typeid 关键字**************/
+
+/******************** decltype 的推导规则*****************************/
+void sample2()
+{
+    //decltype 是在编译期推导类型的，auto是运行期推导的
+    //1. 如果e是一个未加括号的标识符表达式（结构化绑定除外）或者未加括号的类成员访问，则decltype(e)推断出的类型是e的类型T。如果并不存在这样的类型，或者e是一组重载函数，则无法进行推导。
+    //2.如果e是一个函数调用或者仿函数调用，那么de - cltype(e)推断出的类型是其返回值的类型。
+    //3.如果e是一个类型为T的左值，则decltype(e)是T & 。
+    //4.如果e是一个类型为T的将亡值，则decltype(e)是T && 。
+    //5.除去以上情况，则decltype(e)是T。
+
+    int i;
+    int* j;
+    int n[10];
+    const int&& foo();
+    struct A 
+    {
+        int operator()()
+        {
+            return 0;
+        }
+    } a;
+
+    cout << typeid(decltype(static_cast<short>(i))).name() << endl;     //short
+    cout << typeid(decltype(j)).name() << endl;                         //int *
+    cout << typeid(decltype(foo)).name() << endl;                       //int const && (void)
+    cout << typeid(decltype(a())).name() << endl;                       //int 
+    cout << typeid(decltype(i=0)).name() << endl;                       //int 
+    cout << typeid(decltype(n)).name() << endl;                         //int[10]
+    cout << typeid(decltype(*j)).name() << endl;                        //int
+    cout << typeid(decltype(++i)).name() << endl;                       //int
+    cout << typeid(decltype("hellow world")).name() << endl;            //char const[13]
+}
+/******************** decltype 的推导规则*****************************/
+
+int main()
+{
+    //sample1();
+    sample2();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ C++Modern/003右值引用/003右值引用.cpp
@@ -0,0 +1,186 @@
+﻿// 003右值引用.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include<vector>
+using namespace std;
+
+/*****************右值引用测试****************/
+class X
+{
+    bool isCopy;
+public:
+    X()
+    {
+        cout << "X ctor" << endl;
+        isCopy = false;
+        i = 0;
+    }
+    X(X const& x)
+    {
+        cout << "X copy ctor" << endl;
+        isCopy = true;
+        i = 0;
+    }
+    ~X()
+    {
+        cout << (isCopy ? "X copy dtor" : "X dtor") << endl;
+    }
+    int i;
+};
+
+
+X make_x1()
+{
+    return X();
+}
+
+X make_x2()
+{
+    X x;
+    return x;
+}
+
+void sample1()
+{
+    /*左值和右值的定义
+    * 表达式中能用&（取地址符）取得地址的为左值，不能取得地址的为右值
+    * 右值的生命周期只有一瞬间，除非是被特定类型“获取”的用两种手段延长（临时量实质化和两种类型）
+    */
+
+    //临时量实质化手段
+    int i = X().i;  //临时量实质化 S()作为纯右值转化为将亡值后访问变量i
+
+    //能够获取右值延长右值的生命周期的两种类型手段
+    int const& i1 = 0;  //<type> const& 常引用类型
+    int&& i2 = 0;       //<type>&&      右值引用类型
+
+    //右值引用无法获得左值
+    //int&& i3 = i; 报错：无法将右值绑定到左值
+
+
+    //麻了，C++ 11 以后RVO返回值优化被强制启用还关不了，实验结果和理论结果不一致
+
+
+    //直接在返回语句中创建对象
+    {
+        cout << "x1 = make_x1()" << endl;
+        X x1 = make_x1();           //构造1次
+    }
+
+    //返回语句中返回函数作用域内的返回值同类型对象
+    {
+        cout << "\nx2 = make_x2()" << endl;
+        X x2 = make_x2();           //构造1次、复制构造1次
+    }
+
+    //函数产生的返回值为右值，无法用一般引用变量(左值引用)去接
+    //X& x = make_x1();
+    //X& x = make_x2();
+
+    {
+        cout << "\nx3 = make_x2()" << endl;
+        X const& x3 = make_x2();    //构造1次、复制构造1次
+    }
+
+    {
+        cout << "\nx4 = make_x2()" << endl;
+        X&& x4 = make_x2();
+    }
+
+}
+/*****************右值引用测试****************/
+
+/***************************函数形参左值右值测试******************************/
+
+//无论一个函数的实参是左值还是右值，其形参都是一个左值，即使这个形参看上去是一个右值引用
+X forwaord_x(X&& x)
+{
+    return x;
+}
+
+void sample2()
+{
+
+    X xTemp;
+    {
+        //X&& x = forwaord_x(xTemp); //无法将右值转换为左值
+        cout << "\nforwaord_x( X() )" << endl;
+        X&& x1 = forwaord_x(X());    //依旧构造了3次,即使forwaord_x的形参是右值引用
+    }
+
+    {
+        cout << "\nforword_x( static_cast<X&&>(xTemp) )" << endl;
+        X&& x = forwaord_x(static_cast<X&&>(xTemp));
+    }
+
+    {
+        cout << "\nforword_x( std::move(xTemp) )" << endl;
+        X&& x = forwaord_x(move(xTemp));    //move()等效于static_cast<Type&&>()
+    }
+
+    
+}
+/***************************函数形参左值右值测试******************************/
+
+
+/********************完美转发***********************/
+/*
+* 右值引用模板类型在递归推导时会折叠退化成左值引用，
+* 为了确保推导正确应在模板中间层使用forward<T>来根据
+* 本层实际推导类型的左右值属性来将值转换成准确的左值
+* 或者右值。
+*/
+
+template<typename T>
+void show_type(T t)
+{
+    cout << typeid(t).name() << endl;
+}
+
+template<typename T>
+void perfect_forword_x1(T&& x)
+{
+    cout << "show_type(static_cast<T&&>(x))" << endl;
+    show_type(static_cast<T&&>(x));
+}
+
+template<typename T>
+void perfect_forword_x2(T&& x)
+{
+    cout << "show_type(forward<T>(x))" << endl;
+    show_type(forward<T>(x));   //std::forward<T>(x) 等效于 static_cast<T&&>(x)
+}
+
+void sample3()
+{
+    cout << "\nperfect_forword_x1( X() )" << endl;
+    perfect_forword_x1( X() );
+
+    cout << "\nperfect_forword_x2( X() )" << endl;
+    perfect_forword_x2( X() );
+
+    cout << "\nperfect_forword_x2( x )" << endl;
+    X x;
+    perfect_forword_x2(x);
+}
+
+/********************完美转发***********************/
+
+int main()
+{
+    //sample1();
+    //sample2();
+    sample3();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
